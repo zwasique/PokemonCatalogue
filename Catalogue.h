782e1bee6fb84b7d24dd9de5818725ceb1257b5d
zarawasique <zarawasique@gmail.com>
2023-10-20 23:57:14 -0400

    made changes to catalogue.h
    added iterators and const iterators, created default constructor and copy constructor

--- Catalogue.h
+++ Catalogue.h
@@ -12,11 +12,34 @@ class Catalogue {
 public:
     using PokemonCatalogue = std::list<Pokemon>;
 
+    using iterator = PokemonCatalogue::iterator;
+    using const_iterator = PokemonCatalogue::const_iterator;
+
 
 private:
 
+    /**
+     * All pokemen in this 'catalogue'
+     */
+    PokemonCatalogue allPokemon;
+
+public:
+    Catalogue();
 
 
+    /**
+     * When sorting a catalogue, a copy should be made for sorting
+     * and that sorted catalogue will be returned. That way,
+     * the original catalogue is unaffected by the sort
+     * @param src
+     */
+    Catalogue(const Catalogue& src);
+
+    iterator begin();
+    iterator end();
+
+    const_iterator begin() const;
+    const_iterator end() const;
 
 
 };
