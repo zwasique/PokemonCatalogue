355e6bd9be12fb936ee05efcee7e09610ace0235
Zara-Wasique <zarawasique@gmail.com>
2023-11-19 11:07:51 -0500

    implemented iterator support for Catalogue.h

---
+++ Catalogue.cpp
@@ -0,0 +1,19 @@
+#include "Catalogue.h"
+
+
+Catalogue::iterator Catalogue::begin()
+{
+    return allPokemon.begin();
+}
+Catalogue::iterator Catalogue::end()
+{
+    return allPokemon.end();
+}
+Catalogue::const_iterator Catalogue::begin() const
+{
+    return allPokemon.begin();
+}
+Catalogue::const_iterator Catalogue::end() const
+{
+    return allPokemon.end();
+}
