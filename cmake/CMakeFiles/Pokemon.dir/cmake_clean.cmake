7d638e2ce030f7ce185f696a312c4e67d05c6189
Zara-Wasique <zarawasique@gmail.com>
2023-11-21 22:32:06 -0500

    made directories cmake for CMake files and misc for text files used for project ideas

---
+++ cmake/CMakeFiles/Pokemon.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pokemon.dir/Catalogue.cpp.o"
+  "CMakeFiles/Pokemon.dir/Catalogue.cpp.o.d"
+  "CMakeFiles/Pokemon.dir/Pokemon.cpp.o"
+  "CMakeFiles/Pokemon.dir/Pokemon.cpp.o.d"
+  "CMakeFiles/Pokemon.dir/PokemonCatalogue.cpp.o"
+  "CMakeFiles/Pokemon.dir/PokemonCatalogue.cpp.o.d"
+  "Pokemon"
+  "Pokemon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pokemon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
