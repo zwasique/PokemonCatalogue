341a64869e3c378138b5a4ede82e94e1e60c3e68
Zara-Wasique <zarawasique@gmail.com>
2023-11-27 08:35:05 -0500

    started applying menu-idea.txt to main function PokemonCatalogue.cpp

--- src/PokemonCatalogue.cpp
+++ src/PokemonCatalogue.cpp
@@ -1,20 +1,57 @@
 #include <iostream>
-#include <vector>
+#include <set>
+#include <string>
 #include "../include/Pokemon.h"
 #include "../include/Catalogue.h"
 
-//void menu();
+//Function declarations
+void readFiles(std::set<std::string> filesToRead, char instruction, Catalogue catalogue);
+void pokemonSortMenu(Catalogue catalogue);
 
-int main (int argc, char** argv) {
-    if (argc <2){
-        std::cerr << "Supply at least one Pokemon*.txt file";
-    }
+int main () {
 
+    char direction = '?';
+    std::set<std::string> chosenFiles;
+    Catalogue pokemonCatalogue;
 
-    std::vector<std::string> files;
-    files.reserve(argc);
-    for (int i = 0; i < argc; i++){
-        files.emplace_back(argv[i]);}
+    readFiles(chosenFiles, direction, pokemonCatalogue);
+    pokemonSortMenu(pokemonCatalogue);
+
 
 
 }
+
+
+void readFiles(std::set<std::string> filesToRead, char instruction, Catalogue catalogue){
+
+    std::cout << "Which Pokemon generations do you wish to include?"
+                 "Enter a number (1-9), F to finish entering, or E to exit the program: " << std::endl;
+
+    std::cin >> instruction;
+
+    while ((instruction >= '1' && instruction <= '9') ||instruction == 'F' || instruction == 'E'){
+        if (instruction == 'E'){
+            exit(0);}
+
+        while (instruction >= '1' && instruction <= '9'){
+            std::string fileName = "Pokemon" + std::string(reinterpret_cast<const char *>(instruction)) + ".txt";
+            filesToRead.insert(fileName);
+
+            std::cout << "Enter a number (1-9), F to finish entering files, or E to exit the program." << std::endl;
+            std::cin >> instruction;
+        }
+    }
+
+
+
+
+}
+
+void pokemonSortMenu(){
+
+    std::cout << "temp" << std::endl;
+
+
+
+}
+//Function definitions
