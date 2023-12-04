9b7575403448d145c35e59bceebb3e18c86decf2
Zara-Wasique <zarawasique@gmail.com>
2023-12-04 14:55:48 -0500

    added new function so all generations can be added to chosenFiles at once if desired

--- src/PokemonCatalogue.cpp
+++ src/PokemonCatalogue.cpp
@@ -6,6 +6,7 @@
 
 //Function declarations
 void chooseFiles(std::set<std::string> filesToRead, std::string instruction);
+void fillAll(std::set<std::string> fileToRead);
 void pokemonSortMenu(Catalogue catalogue);
 
 int main () {
@@ -29,12 +30,14 @@ int main () {
 void chooseFiles(std::set<std::string> filesToRead, std::string instruction){ //instruction has already been initialised to an invalid value
     while (instruction != "F"){
 
-        if (instruction == "E"){
-            exit(0);
 
+        if (instruction == "A"){
+            fillAll(filesToRead);
         } else if (instruction >= "1" && instruction <= "9") {
             std::string fileName = "Pokemon" + instruction + ".txt";
             filesToRead.insert(fileName);
+        } else if (instruction == "E") {
+                exit(0);
         }
 
         std::cout << "Enter a number (1-9), F to finish entering files, or E to exit the program." << std::endl;
@@ -45,6 +48,13 @@ void chooseFiles(std::set<std::string> filesToRead, std::string instruction){ //
 
 }
 
+void fillAll(std::set<std::string> fileToRead){
+    for (int i = 1; i < 10; i ++){
+        std::string fileName = "Pokemon" + std::to_string(i) + ".txt";
+        fileToRead.insert(fileName);
+    }
+
+}
 
 void pokemonSortMenu(){
 
