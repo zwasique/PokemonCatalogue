2f1301162b94c2de39b0056fbb1fe72e76cc5b2d
Sean Allgaier <146116441+seanallgaier@users.noreply.github.com>
2023-12-24 11:05:20 -0500

    Updating Catalogue.cpp in order to attempt to fix the filesystem errors

--- src/Catalogue.cpp
+++ src/Catalogue.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <filesystem>
 
-namespace fs = std::__fs::filesystem;
+//namespace fs = std::__fs::filesystem;
 
 Catalogue::iterator Catalogue::begin()
 {
@@ -27,11 +27,11 @@ Catalogue::PokemonCatalogue Catalogue::getAllPokemon() {
 
 void Catalogue::fillCatalogue(const std::set<std::string>& generationFiles){
     //std::cerr<< "void Catalogue::fillCatalogue(const std::setMstd::string>& generationFiles)";
-    fs::path parentPath = "..";
-    fs::path withDirectory = parentPath / "pokemonGenerationFiles";
+    std::filesystem::path parentPath = "..";
+    std::filesystem::path withDirectory = parentPath / "pokemonGenerationFiles";
 
     for (const std::string& file: generationFiles) {
-        fs::path fullPath = withDirectory / file;
+        std::filesystem::path fullPath = withDirectory / file;
         //std::string stringPath = fullPath.string();
         std::ifstream fileStream(fullPath);
 
